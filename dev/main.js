22cbf229f43c442e071bdd61b4945cb59d42f2b8
Renan Oliveira <ren4n.oliveir4@gmail.com>
2017-03-30 17:01:01 -0300

    Add language Portuguese Brazil

--- dev/main.js
+++ dev/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import vueEventCalendar from '../src/'
 // import vueEventCalendar from '../dist'
 // import '../dist/style.css'
-Vue.use(vueEventCalendar, {locale: 'es'})
+Vue.use(vueEventCalendar, {locale: 'pt-br'})
 
 new Vue({
   el: '#app',
