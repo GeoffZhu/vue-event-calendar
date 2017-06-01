2c506fc1c69cbdf4c5695d7adb1f63b53cceea51
shigg1120 <knowledgedeliver@gmail.com>
2017-06-02 06:25:15 +0900

    set locale

--- dev/main.js
+++ dev/main.js
@@ -4,7 +4,7 @@ import App from './App.vue'
 import vueEventCalendar from '../src/'
 // import vueEventCalendar from '../dist'
 // import '../dist/style.css'
-Vue.use(vueEventCalendar, {locale: 'pt-br'})
+Vue.use(vueEventCalendar, {locale: 'ja'})
 
 new Vue({
   el: '#app',
