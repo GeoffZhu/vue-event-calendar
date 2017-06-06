1820c176be50f754f597d498b8f14d8e58d54a13
朱陶然 <geoffzhu1992@gmail.com>
2017-06-06 11:29:29 +0800

    add language ko

--- src/i18n.js
+++ src/i18n.js
@@ -38,5 +38,13 @@ export default {
     fullFormat: 'yyyy/MM/dd',
     dayEventsTitle: '全てのイベント',
     notHaveEvents: 'イベントはありません'
+  },
+  ko: {
+    dayNames: ["일", "월", "화", "수", "목", "금", "토"],
+    monthNames: ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"],
+    format: 'yyyy/MM',
+    fullFormat: 'yyyy/MM/dd',
+    dayEventsTitle: '모든 이벤트',
+    notHaveEvents: '일정이 없습니다'
   }
 }
