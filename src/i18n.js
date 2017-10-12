3dd3990bc87f213d6a6e57159abda67d3aa8fecc
朱陶然 <geoffzhu1992@gmail.com>
2017-10-12 10:39:41 +0800

    add 'us' locale

--- src/i18n.js
+++ src/i18n.js
@@ -15,6 +15,14 @@ export default {
     dayEventsTitle: '全部事件',
     notHaveEvents: '没有事件'
   },
+  us: {
+    dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    format: 'MM/yyyy',
+    fullFormat: 'MM/dd/yyyy',
+    dayEventsTitle: 'All Events',
+    notHaveEvents: 'Not Have Events'
+  },
   es: {
     dayNames: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sá"],
     monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
