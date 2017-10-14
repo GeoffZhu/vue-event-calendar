c4432a5d3135e2e32ad15e00984f5617a4178c4e
Anh Mai <theanht1k20@gmail.com>
2017-10-14 15:30:15 +0700

    Add 'vi' locale

--- src/i18n.js
+++ src/i18n.js
@@ -94,5 +94,13 @@ export default {
     fullFormat: 'dd.MM.yyyy',
     dayEventsTitle: 'Alle Veranstaltungen',
     notHaveEvents: 'Keine Veranstaltungen'
+  },
+  vi: {
+    dayNames: ["T2", "T3", "T4", "T5", "T6", "T7", "CN"],
+    monthNames: ["Tháng 1", "Tháng 2", "Tháng 3", "Tháng 4", "Tháng 5", "Tháng 6", "Tháng 7", "Tháng 8", "Tháng 9", "Tháng 10", "Tháng 11", "Tháng 12"],
+    format: 'MM/yyyy',
+    fullFormat: 'dd/MM/yyyy',
+    dayEventsTitle: 'Tất cả sự kiện',
+    notHaveEvents: 'Không có sự kiện nào'
   }
 }
